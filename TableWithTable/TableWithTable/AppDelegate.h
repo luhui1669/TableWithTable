b1a3cafc06cfb867c00e24a2eace0e332135b397
XTShow <447800853@qq.com>
2016-06-07 09:08:02 +0800

    初始化项目。这是一颗种子^.^

---
+++ TableWithTable/TableWithTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableWithTable
+//
+//  Created by 肖霆 on 16/6/7.
+//  Copyright © 2016年 XTShow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableWithTable/TableWithTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableWithTable
+//
+//  Created by 肖霆 on 16/6/7.
+//  Copyright © 2016年 XTShow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
