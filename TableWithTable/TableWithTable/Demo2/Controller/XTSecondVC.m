806173adf54354781e37356579a38f694c391f1f
XTShow <447800853@qq.com>
2016-06-09 12:48:48 +0800

    no message

--- TableWithTable/TableWithTable/Demo2/Controller/XTSecondVC.m
+++ TableWithTable/TableWithTable/Demo2/Controller/XTSecondVC.m
@@ -25,7 +25,6 @@
     self.rightView = [[XTSecondRightTableView alloc] init];
     
     self.leftView.delegate = self;
-//    self.rightView.delegate = self;
     
     [self setupUI];
     [self setupData];
@@ -34,9 +33,19 @@
 -(void)setupUI{
     self.edgesForExtendedLayout = UIRectEdgeNone;
     
-    self.leftView.frame = CGRectMake(0, 0, kScreenWidth/4, kTableViewMaxH);
-    self.rightView.frame = CGRectMake(kScreenWidth/4, 0, kScreenWidth/4 * 3, kTableViewMaxH);
+    //给左右两个tableView设置Frame(默认情况下：leftView宽度为屏幕的1/4，rightView宽度为屏幕的3/4，高度都为全屏)
+    if (CGRectIsEmpty(self.leftFrame)) {
+        self.leftView.frame = CGRectMake(0, 0, kScreenWidth/4, kTableViewMaxH);
+    }else{
+        self.leftView.frame = self.leftFrame;
+    }
     
+    if (CGRectIsEmpty(self.rightFrame)) {
+        self.rightView.frame = CGRectMake(kScreenWidth/4, 0, kScreenWidth/4 * 3, kTableViewMaxH);
+    }else{
+        self.rightView.frame = self.rightFrame;
+    }
+
     self.rightView.sectionHeaderH = 30;
     self.rightView.cellHeight = 44;
     
@@ -61,8 +70,7 @@
 #pragma mark -
 #pragma mark tableView的代理方法
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (tableView == self.leftView) {
-        
+    
     self.leftView.isSelect = 1;
     self.rightView.leftTableView = self.leftView;
         
@@ -76,7 +84,7 @@
         
             CGRect frame = CGRectZero;
             frame.size.height = self.rightView.bounds.size.height - self.rightView.sectionHeaderH -  array.count * self.rightView.cellHeight + 0.14;
-            //在点击左侧最后一组后，再上拉右侧tableView，会调用右侧tableView倒数第二个组的willDisplayHeaderView，尝试许久没有结果，只能加0.14解决（为什么是0.14？因为是我逐步实验的。。。。最后得到的范围是0.139不行，0.14可以，没继续往下实验。。。）估计这个0.14是willDisplayHeaderView中对will的判定值；
+            //在点击左侧最后一组后，再上拉右侧tableView，会调用右侧tableView倒数第二个组的willDisplayHeaderView，尝试许久没有解决，只能加0.14解决（为什么是0.14？我逐步试验的。。。。最后得到的范围是0.139～0.14，没继续往下试验。。。）估计这个0.14是willDisplayHeaderView中对will的判定值；
             //在逻辑上如何屏蔽掉暂时还没想出来。。。估计可以用手势动作的方法解决～可是。。。我不太会。。。哈哈哈～～～未来加强一下！
 
             UIView *view = [[UIView alloc]initWithFrame:frame];
@@ -85,6 +93,6 @@
         }
         }
     }
-}
+
 
 @end
